d4bd80d7748228100577ff2b721453ca66302bdd
James Harris <mharris9608@yahoo.com>
2020-12-03 00:36:55 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,3 +9,22 @@ def position_taken?(board, index)
 board[index] == " " || board[index] == "" || board[index] == nil
 board[index] == "X" || board[index] == "O"
 end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  converted_input = (user_input.to_i) - 1
+end
+
+def move(board, input, value = 'X')
+  def update_array_at_with(board, input, value = 'X')
+    board[input] = value
+  end
+  update_array_at_with(board, input, value)
+end
